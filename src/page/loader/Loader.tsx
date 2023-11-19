bfb925429dcb2e1f5d30860d9574b7fc8e6b39db
karolina <k.banasiewicz@hyperting.com>
2023-11-19 23:06:16 +0100

    check

--- src/page/loader/Loader.tsx
+++ src/page/loader/Loader.tsx
@@ -1,16 +1,20 @@
-import { Text,Flex, Spinner } from "@chakra-ui/react";
+import { Text, Flex, Spinner } from "@chakra-ui/react";
 
-
-    export   const Loader = () => (
-    <Flex flexDir={'column'} justifyContent={'center'} h='100vh' alignItems={'center'}>
-      <Text>loading PAGE...</Text>
-      <Spinner
-        thickness='4px'
-        speed='0.85s'
-        emptyColor='gray.700'
-        color='blue.500'
-        size='xl'
-        mt={10}
-      />
-    </Flex>
-  )
+export const Loader = () => (
+  <Flex
+    flexDir={"column"}
+    justifyContent={"center"}
+    h="100vh"
+    alignItems={"center"}
+  >
+    <Text>loading PAGE...</Text>
+    <Spinner
+      thickness="4px"
+      speed="0.85s"
+      emptyColor="gray.700"
+      color="blue.500"
+      size="xl"
+      mt={10}
+    />
+  </Flex>
+);
